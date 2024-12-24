c3583a525ec5994e6181bbdd18ec9f62ed84e91d
FullStackPete <psliwa.b@gmail.com>
2024-12-24 02:05:22 +0100

    refactor: refactor Box components in House and Carpet

--- src/components/Carpet.tsx
+++ src/components/Carpet.tsx
@@ -5,9 +5,14 @@ import { Box } from "./Box";
 export function Carpet(): ReactElement {
   return (
     <>
-      <Box position={[0, 0.2, 0]} form={[3, 0.05, 3]} color={"#474463"} />
-      <Box position={[0, 0.2, 0]} form={[3.1, 0.05, 3]} color={"#474463"} />
-      <Box position={[0, 0.2, 0]} form={[3, 0.05, 3.1]} color={"#474463"} />
+      <Box position={[0, 0.155, 0]} form={[3, 0.05, 3]} color={"#474463"} />
+      <Box
+        castShadow
+        position={[0, 0.155, 0]}
+        form={[3.1, 0.05, 3]}
+        color={"#474463"}
+      />
+      <Box position={[0, 0.155, 0]} form={[3, 0.05, 3.1]} color={"#474463"} />
     </>
   );
 }
--- src/components/House.tsx
+++ src/components/House.tsx
@@ -3,30 +3,47 @@ import { Box } from "./Box";
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 import { Carpet } from "./Carpet";
 import { Paintings } from "./Paintings";
+import { Desk } from "./Desk";
 
 export function House(): ReactElement {
   return (
     <group position={[0, 0, 0]}>
-      <Box position={[0, 0.1, 0]} form={[4, 0.1, 6]} color={"#422f2d"} />
-      <Box position={[0, 1, 3]} form={[4, 1.9, 0.1]} color={"#422f2d"} />
-      <Box position={[2, 1, 0]} form={[0.1, 1.9, 6]} color={"#422f2d"} />
+      <Box
+        receiveShadow
+        position={[0, 0.1, 0]}
+        form={[4, 0.1, 6]}
+        color={"#422f2d"}
+      />
+      <Box
+        receiveShadow
+        position={[0, 1, 3]}
+        form={[4, 1.9, 0.1]}
+        color={"#422f2d"}
+      />
+      <Box
+        receiveShadow
+        position={[2, 1, 0]}
+        form={[0.1, 1.9, 6]}
+        color={"#422f2d"}
+      />
       <Carpet />
-      <Paintings/>
-      <AccumulativeShadows
-        frames={80}
-        color="black"
-        opacity={1}
-        scale={12}
-        position={[0, 0.04, 0]}
-      >
-        <RandomizedLight
-          amount={8}
-          radius={5}
-          ambient={0.5}
-          position={[5, 6, -10]}
-          bias={0.001}
-        />
-      </AccumulativeShadows>
+      <Paintings />
+      <Desk />
+      {/* <AccumulativeShadows */}
+      {/*   frames={80} */}
+      {/*   color="black" */}
+      {/*   opacity={1} */}
+      {/*   scale={12} */}
+      {/*   position={[0, 0.04, 0]} */}
+      {/* > */}
+      {/*   <RandomizedLight */}
+      {/*     amount={8} */}
+      {/*     radius={5} */}
+      {/*     ambient={0.5} */}
+      {/*     position={[5, 6, -10]} */}
+      {/*     bias={0.001} */}
+      {/*   /> */}
+      {/* </AccumulativeShadows> */}
     </group>
   );
 }
