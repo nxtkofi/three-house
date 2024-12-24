7d10bc73976dda91be5d54548c19fed1120eb661
FullStackPete <psliwa.b@gmail.com>
2024-12-24 02:05:39 +0100

    feat: add new lightning

--- src/components/BasicEnvironment.tsx
+++ src/components/BasicEnvironment.tsx
@@ -1,5 +1,6 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { type ReactElement, type ReactNode } from "react";
+import { Box } from "./Box";
 
 export interface BasicEnvironmentProps {
   children: ReactNode;
@@ -8,15 +9,19 @@ export interface BasicEnvironmentProps {
 export function BasicEnvironment(props: BasicEnvironmentProps): ReactElement {
   return (
     <>
-      <color attach="background" args={["#f0f0f0"]} />
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight decay={2} position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight decay={0} position={[10, 8, -5]} intensity={6} />
-      <Environment
-        files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/dancing_hall_1k.hdr"
-        background
-        blur={1}
+      <color attach="background" args={["#000c10"]} />
+      <ambientLight position={[5, 5, 0]} />
+      <spotLight
+        castShadow
+        intensity={100}
+        decay={1.5}
+        position={[5, 5, 0]}
+        angle={0.59244}
+        penumbra={1.1}
+        color="#fcf6ff"
       />
+      {/* TODO: remove below box as it's representation of light's placement */}
+      <Box position={[5, 5, 0]} color="#ffffff" />
       <OrbitControls makeDefault />
       {props.children}
     </>
